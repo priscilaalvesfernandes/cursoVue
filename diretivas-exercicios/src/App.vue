61c7bc3901d1935fef9e15308938d7dcea4a6948
priscilaalvesfernandes <priscila_eld@hotmail.com455cl>
2022-03-28 07:26:33 -0400

    updt1

---
+++ diretivas-exercicios/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<h1>Diretivas</h1>
+		<hr>
+		<p v-text="'Usando diretivas v-text'"></p>
+		<p v-html="'Usando diretivas <strong>v-html</strong>'"></p> <!-- usando tags html dentro de js -->
+		<hr>
+		<p v-destaque:fundo.atrasar="'lightblue'">Diretiva personalizada</p>
+		<p v-destaque="cor">Diretiva personalizada</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+			cor: 'green'
+		}
+	}
+	
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
---
+++ diretivas-exercicios/src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.directive('destaque',{ //criando diretivas
+	bind(el, binding) { // binding, vnode caso nao precise pode ser retirado 
+		// el.style.backgroundColor = 'lightgreen'
+
+		let atraso = 0 //modificando diretiva
+
+		if(binding.modifiers ['atrasar']) atraso = 3000
+
+		setTimeout(() => {
+			if(binding.arg === 'fundo'){ //passando argumento para diretiva
+				el.style.backgroundColor = binding.value
+			}else{
+				el.style.color = binding.value
+			}
+		}, atraso)
+
+		
+		// if(binding.arg === 'fundo'){ //passando argumento para diretiva
+		// 	el.style.backgroundColor = binding.value
+		// }else{
+		// 	el.style.color = binding.value
+		}
+
+	}
+)
+
+new Vue({
+	render: h => h(App),
+}).$mount('#app')
