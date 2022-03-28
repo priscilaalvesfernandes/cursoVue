875c31ccfe85516c3f2a08cd18570a4de92e1d81
priscilaalvesfernandes <priscila_eld@hotmail.com455cl>
2022-03-28 11:31:25 -0400

    updt 8 desafio mixin

--- filtros-mixins-desafio/src/App.vue
+++ filtros-mixins-desafio/src/App.vue
@@ -3,14 +3,18 @@
 		<h1>Filtros & Mixins (Desafio)</h1>
 		<!-- Exercício 1 -->
 		<!-- Construir um filtro local que troca espaços por vírgula -->
+		<p> {{ frase | espacoPorVirgula}} </p>
 		
 		<!-- Exercício 2 -->
 		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
 			valor na string final -->
 		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+		<p> {{ frase | contarPalavra}} </p>
 
 		<!-- Exercício 3 -->
 		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
+		<p> {{ fraseComVirgula }} </p>
+		<p> {{ fraseComTamanho }} </p>
 
 		<!-- Exercício 4 -->
 		<!-- Compartilhe a propriedade computada via mixin -->
@@ -18,8 +22,21 @@
 </template>
 
 <script>
+import fraseMixim from './fraseMixim'
+
+
 export default {
-	
+	mixins: [fraseMixim],
+	data() {
+		return {
+			frase: 'Frase pra teste de filtro.'
+		}
+	},
+	filters: {
+		espacoPorVirgula(valor){ 
+			return valor.replace(/ /g, ',')
+		}
+	},
 }
 </script>
 
