addc1773d464be17da48f489698bd12ce25e6385
priscilaalvesfernandes <priscila_eld@hotmail.com455cl>
2022-03-28 10:15:43 -0400

    updt6

--- filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins-exercicios/src/App.vue
@@ -1,12 +1,29 @@
 <template>
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p> {{ cpfDoAluno | cpf | inverter}} </p>
+		<input type="text" :value="cpfDoAluno | cpf">
 	</div>
 </template>
 
 <script>
 export default {
+	filters: { //criar filtro local
+		cpf(valor) {
+			const arr = `${valor}`.split('')//quebra  string e coloca cada caractere dentro de um array
+			arr.splice(3,0,'.') //add caractere "." na posicao 3
+			arr.splice(7,0,'.') //add caractere "." na posicao 6+1 pois ao add . aumenta a quantodade de caractere da string
+			arr.splice(11,0,'-')
+			return arr.join('')
+		}
+	},
 
+	data() {
+		return {
+			cpfDoAluno: '12345678910'
+		}
+	}
 }
 </script>
 
@@ -18,6 +35,10 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+input {
 	font-size: 2.5rem;
 }
 </style>
